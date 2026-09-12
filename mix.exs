a2f4e70e80aa664cddf4da7a3fc96f8106206f70
项目维护者 <project-maintainer@local.invalid>
2026-09-12 22:17:32 +0800

    明确后端依赖与开发约定

--- mix.exs
+++ mix.exs
@@ -1,3 +1,6 @@
-defmodule BackendService.MixProject do
+defmodule DomainBackend.MixProject do
   use Mix.Project
+  def project, do: [app: :domain_backend, version: "0.1.0", elixir: "~> 1.17", deps: deps()]
+  def application, do: [extra_applications: [:logger]]
+  defp deps, do: [{:phoenix, "~> 1.7"}, {:postgrex, ">= 0.0.0"}, {:redix, "~> 1.5"}]
 end
